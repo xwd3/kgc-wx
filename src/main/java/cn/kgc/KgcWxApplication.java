2964e2d192997aa7f90827a6317ca5854a2c3452
xwd3 <15513897816@163.com>
2019-09-08 00:06:17 +0800

    提交微信代码1

---
+++ src/main/java/cn/kgc/KgcWxApplication.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgcWxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcWxApplication.class, args);
+	}
+
+}
